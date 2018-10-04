65b1a92c8f705d1a407d22a8cb404630cdf9a0fb
msicakkanli <msicakkanli@protein.tech>
2018-10-04 21:04:54 +0300

    add stcokmarket

--- routes/index.js
+++ routes/index.js
@@ -31,6 +31,7 @@ router.get('/', function (req, res, next) {
        item.selling = newSellig
        item.change_rate = newChange
       });
+    
       let usds = usd.data
       let eurs = eur.data
       let gbps = gbp.data
@@ -45,58 +46,117 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/crypto', function (req, res, next) {
-
-  
   function getBtc() {
     let cryptoApi = 'https://www.doviz.com/api/v1/coins/bitcoin/latest'
-     return axios.get(cryptoApi)
-   }
-   function getEth() {
-     let cryptoApi = 'https://www.doviz.com/api/v1/coins/ethereum/latest'
-     return axios.get(cryptoApi)
-   }
-   function getXrp() {
-     let cryptoApi = 'https://www.doviz.com/api/v1/coins/ripple/latest'
-     return axios.get(cryptoApi)
-   }
-   function getLtc() {
+    return axios.get(cryptoApi)
+  }
+  function getEth() {
+    let cryptoApi = 'https://www.doviz.com/api/v1/coins/ethereum/latest'
+    return axios.get(cryptoApi)
+  }
+  function getXrp() {
+    let cryptoApi = 'https://www.doviz.com/api/v1/coins/ripple/latest'
+    return axios.get(cryptoApi)
+  }
+  function getLtc() {
     let cryptoApi = 'https://www.doviz.com/api/v1/coins/litecoin/latest'
     return axios.get(cryptoApi)
   }
-   function getAll() {
-     let allApi = 'https://www.doviz.com/api/v1/coins/all/latest'
-     return axios.get(allApi)
-   }
-   axios.all([getBtc(), getEth(), getXrp(),getLtc(), getAll()])
-     .then(axios.spread(function (btc, eth, xrp ,ltc, all) {
-       let alls = all.data
-       alls.forEach(function(item){
+  function getAll() {
+    let allApi = 'https://www.doviz.com/api/v1/coins/all/latest'
+    return axios.get(allApi)
+  }
+  axios.all([getBtc(), getEth(), getXrp(), getLtc(), getAll()])
+    .then(axios.spread(function (btc, eth, xrp, ltc, all) {
+      let alls = all.data
+      alls.forEach(function (item) {
         var newSellig = Math.round(item.selling * 100) / 100
         var newChange = Math.round(item.change_rate * 100) / 100
         item.selling = newSellig
         item.change_rate = newChange
+      });
+      const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2
+      })
+      alls.forEach(function(item){
+        var newSellig = formatter.format(item.selling)
+        var newChange = Math.round(item.change_rate * 100) / 100
+        var newVolume = formatter.format(item.market_cap)
+        item.selling = newSellig
+        item.change_rate = newChange
+        item.market_cap = newVolume
        });
-       let btcs = btc.data
-       let eths = eth.data
-       let xrps = xrp.data
-       let ltcs = ltc.data
-       let btcBuy = Math.round(btcs.selling * 100) / 100
-       let btcChange = Math.round(btcs.change_rate * 100) / 100
-       let btcVolume = btcs.volume
-       let ethBuy = Math.round(eths.selling * 100) / 100
-       let ethChange = Math.round(eths.change_rate * 100) / 100
-       let ethVolume = eths.volume
-       let xrpBuy = Math.round(xrps.selling * 100) / 100
-       let xrpChange = Math.round(xrps.change_rate * 100) / 100
-       let xrpVolume = xrps.volume
-       let ltcBuy = Math.round(ltcs.selling * 100) / 100
-       let ltcChange = Math.round(ltcs.change_rate * 100) / 100
-       let ltcVolume = ltcs.volume
-       
-       res.render('crypto', { title: 'Money Tracker', btcBuy:btcBuy, btcChange:btcChange,btcVolume:btcVolume,
-       ethBuy:ethBuy, ethChange:ethChange,ethVolume:ethVolume ,xrpBuy:xrpBuy, xrpChange:xrpChange,xrpVolume:xrpVolume,
-       ltcBuy:ltcBuy, ltcChange:ltcChange,ltcVolume:ltcVolume ,alls:alls });
-     }))
+
+      let btcs = btc.data
+      let eths = eth.data
+      let xrps = xrp.data
+      let ltcs = ltc.data
+      let btcBuy = formatter.format(btcs.selling)
+      let btcChange = Math.round(btcs.change_rate * 100) / 100
+      let btcVolume = formatter.format(btcs.volume)
+      let ethBuy = formatter.format(eths.selling)
+      let ethChange = Math.round(eths.change_rate * 100) / 100
+      let ethVolume = formatter.format(eths.volume)
+      let xrpBuy = formatter.format(xrps.selling)
+      let xrpChange = Math.round(xrps.change_rate * 100) / 100
+      let xrpVolume = formatter.format(xrps.volume)
+      let ltcBuy = formatter.format(ltcs.selling)
+      let ltcChange = Math.round(ltcs.change_rate * 100) / 100
+      let ltcVolume = formatter.format(ltcs.volume)
+      res.render('crypto', {
+        title: 'Money Tracker', btcBuy: btcBuy, btcChange: btcChange, btcVolume: btcVolume,
+        ethBuy: ethBuy, ethChange: ethChange, ethVolume: ethVolume, xrpBuy: xrpBuy, xrpChange: xrpChange, xrpVolume: xrpVolume,
+        ltcBuy: ltcBuy, ltcChange: ltcChange, ltcVolume: ltcVolume, alls: alls
+      });
+    }))
+})
+
+router.get('/stockMarket', function(req,res,next){
+  function get100() {
+    let stockApi = 'http://www.doviz.com/api/v1/indexes/XU100/latest'
+    return axios.get(stockApi)
+  }
+  function get50() {
+    let stockApi = 'http://www.doviz.com/api/v1/indexes/XU050/latest'
+    return axios.get(stockApi)
+  }
+  function get30() {
+    let stockApi = 'http://www.doviz.com/api/v1/indexes/XU030/latest'
+    return axios.get(stockApi)
+  }
+  function getGold() {
+    let goldApi = 'http://www.doviz.com/api/v1/golds/all/latest'
+    return axios.get(goldApi)
+  }
+  axios.all([get100(), get30(), get50(), getGold()])
+    .then(axios.spread(function (x100, x50, x30, gold) {
+      let golds = gold.data
+      const formatter = new Intl.NumberFormat('en-TR', {
+        style: 'currency',
+        currency: 'TRY',
+        minimumFractionDigits: 2
+      })
+      golds.forEach(function(item){
+        var newSellig = formatter.format(item.selling)
+        var newChange = Math.round(item.change_rate * 100) / 100
+        item.selling = newSellig
+        item.change_rate = newChange
+       });
+
+      let x100s = x100.data
+      let x50s = x50.data
+      let x30s = x30.data
+      let x100Change = Math.round(x100s.change_rate * 100) / 100
+      let x50Change = Math.round(x50s.change_rate * 100) / 100
+      let x30Change = Math.round(x30s.change_rate * 100) / 100
+
+      res.render('stockMarket', {
+        title: 'Money Tracker', x100s: x100s, x50s:x50s, x30s:x30s, x100Change:x100Change,x50Change:x50Change,x30Change:x30Change,
+        golds:golds
+      });
+    }))
 })
 
 module.exports = router;
