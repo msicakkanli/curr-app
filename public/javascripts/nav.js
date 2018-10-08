ba818aa1ea0abfd706580195133ef9079225f410
msicakkanli <msicakkanli@protein.tech>
2018-10-08 15:19:55 +0300

    add login form

--- public/javascripts/nav.js
+++ public/javascripts/nav.js
@@ -22,15 +22,5 @@ $(document).ready(function () {
   $('#sell').toFixed(2)
 })
 
-// Toggle Function
-$('.toggle').click(function(){
-  // Switches the Icon
-  $(this).children('i').toggleClass('fa-pencil');
-  // Switches the forms  
-  $('.form').animate({
-    height: "toggle",
-    'padding-top': 'toggle',
-    'padding-bottom': 'toggle',
-    opacity: "toggle"
-  }, "slow");
-});
+
+
